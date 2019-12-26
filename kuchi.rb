0dcc744ce2bb93cc0cd9fb8b761974117a7673c7
reedleoneil <reedleoneilpascual@yahoo.com>
2019-12-27 03:05:44 +0800

    updated kuchi and mimi

--- kuchi.rb
+++ kuchi.rb
@@ -3,15 +3,15 @@ require 'msgpacker'
 require 'ostruct'
 
 client = PahoMqtt::Client.new
-client.connect('localhost', 1883, client.keep_alive, true, true)
-client.subscribe(["#", 2])
+client.connect('broker.hivemq.com', 1883, client.keep_alive, true, true)
+client.subscribe(["/bu/#", 2])
 
-client.add_topic_callback("/bu/agents/#{ARGV[1]}/inators/file_rw/events/read") do |packet|
+client.add_topic_callback("/bu/shinobi/#{ARGV[1]}/inators/filerw/events/read") do |packet|
 	packet = OpenStruct.new MessagePack.unpack(packet.payload)
 	if ARGV[5] then File.binwrite(ARGV[5], packet.data, 0) else puts packet.data end
 end
 
-client.add_topic_callback("/bu/agents/#{ARGV[1]}/inators/remote_shell/events/read") do |packet|
+client.add_topic_callback("/bu/shinobi/#{ARGV[1]}/inators/remoteshell/events/read") do |packet|
 	packet = OpenStruct.new MessagePack.unpack(packet.payload)
 	if ARGV[4] then
 		File.binwrite(ARGV[4], packet.data, File.exists?(ARGV[4]) ? File.size(ARGV[4]) + 1 : 0) 
@@ -23,27 +23,27 @@ end
 case ARGV[0].to_i
 	when 1
 		@p = {
-			topic: "/bu/agents/#{ARGV[1]}/inators/remote_shell/cmds/open",
+			topic: "/bu/shinobi/#{ARGV[1]}/inators/remoteshell/cmds/open",
 			payload: { shell:  ARGV[2] }
 		}
 	when 2
 		@p = {
-			topic: "/bu/agents/#{ARGV[1]}/inators/remote_shell/cmds/close",
+			topic: "/bu/shinobi/#{ARGV[1]}/inators/remoteshell/cmds/close",
 			payload: { pid: ARGV[2].to_i }
 		}
 	when 3
 		@p = {
-			topic: "/bu/agents/#{ARGV[1]}/inators/remote_shell/cmds/write",
+			topic: "/bu/shinobi/#{ARGV[1]}/inators/remoteshell/cmds/write",
 			payload: { pid: ARGV[2].to_i, data: ARGV[3] }
 		}
 	when 4
 		@p = {
-			topic: "/bu/agents/#{ARGV[1]}/inators/file_rw/cmds/read",
+			topic: "/bu/shinobi/#{ARGV[1]}/inators/filerw/cmds/read",
 			payload: { file: ARGV[2], length: ARGV[3].to_i, offset: ARGV[4].to_i }
 		}
 	when 5
 		@p = {
-			topic: "/bu/agents/#{ARGV[1]}/inators/file_rw/cmds/write",
+			topic: "/bu/shinobi/#{ARGV[1]}/inators/filerw/cmds/write",
 			payload: { file: ARGV[2], data: File.binread(ARGV[3]), offset: ARGV[4].to_i }
 		}
 	else
--- mimi.rb
+++ mimi.rb
@@ -2,8 +2,8 @@ require 'paho-mqtt'
 require 'msgpacker'
 
 client = PahoMqtt::Client.new
-client.connect('localhost', 1883, client.keep_alive, true, true)
-client.subscribe(["#", 2])
+client.connect('broker.hivemq.com', 1883, client.keep_alive, true, true)
+client.subscribe(["/bu/#", 2])
 
 client.on_message do |p|
 	puts "Topic: #{p.topic}\nPayload: #{MessagePack.unpack p.payload}\nQoS: #{p.qos}"
