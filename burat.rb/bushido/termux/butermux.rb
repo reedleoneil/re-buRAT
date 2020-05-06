88c10560330218931a004f92c092e5c0a6b7e9eb
reedleoneil <reedleoneilpascual@yahoo.com>
2020-05-06 22:52:44 +0800

    🔴 fixed finding termux by id

--- burat.rb/bushido/termux/butermux.rb
+++ burat.rb/bushido/termux/butermux.rb
@@ -58,68 +58,68 @@ module Bushido
     end
 
     def close(id)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @termuxs.delete termux
       @on_close.call(id)
     end
 
     def audio_info(id)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_audio_info.call(id, termux.audio_info())
     end
 
     def battery_status(id)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_battery_status.call(id, termux.battery_status())
     end
 
     def call_log(id, limit = 10, offset = 0)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_call_log.call(id, termux.call_log(limit, offset))
     end
 
     def camera_info(id)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_camera_info.call(id, termux.camera_info())
     end
 
     def camera_photo(id, camera_id = 0, output_file)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_camera_photo.call(id, termux.camera_photo(camera_id, output_file))
     end
 
     def contact_list(id)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_contact_list.call(id, termux.contact_list())
     end
 
     def sms_list(id, limit = 10, offset = 0, type = 'all')
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_sms_list.call(id, termux.sms_list(limit, offset, type))
     end
 
     def sms_send(id, number, text)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_sms_send.call(id, termux.sms_send(number, text))
     end
 
     def telephony_call(id, number)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_telephony_call.call(id, termux.telephony_call(number))
     end
 
     def device_info(id)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_device_info.call(id, termux.device_info())
     end
 
     def wifi_connection_info(id)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_wifi_connection_info.call(id, termux.wifi_connection_info())
     end
 
     def wifi_scan_info(id)
-      termux = @termuxs.find { |termux| termux.id = id }
+      termux = @termuxs.find { |termux| termux.id == id }
       @on_wifi_scan_info.call(id, termux.wifi_scan_info())
     end
 
