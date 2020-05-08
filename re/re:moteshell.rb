299d9f06a5f111540a13ce5c1aff6da7a671a6b0
reedleoneil <reedleoneilpascual@yahoo.com>
2020-05-08 17:59:18 +0800

    📟 optimized and enhanced opening and closing of remoteshell

--- re/re:moteshell.rb
+++ re/re:moteshell.rb
@@ -29,9 +29,9 @@ re.add_topics({
   :nil                    => "bu/nil",
   :bushi									=> "bu/bushi/#{params[:bushi]}",
   :remoteshell						=> "bu/bushi/#{params[:bushi]}/bushido/remoteshell/#{params[:id]}",
-  :remoteshell_cmd_open		=> "bu/bushi/#{params[:bushi]}/bushido/remoteshell/#{params[:id]}/cmd/open",
-  :remoteshell_cmd_close	=> "bu/bushi/#{params[:bushi]}/bushido/remoteshell/#{params[:id]}/cmd/close",
   :remoteshell_cmd_write	=> "bu/bushi/#{params[:bushi]}/bushido/remoteshell/#{params[:id]}/cmd/write",
+  :remoteshell_evt_open		=> "bu/bushi/#{params[:bushi]}/bushido/remoteshell/#{params[:id]}/evt/open",
+  :remoteshell_evt_close	=> "bu/bushi/#{params[:bushi]}/bushido/remoteshell/#{params[:id]}/evt/close",
   :remoteshell_evt_read		=> "bu/bushi/#{params[:bushi]}/bushido/remoteshell/#{params[:id]}/evt/read",
   :remoteshell_evt_write	=> "bu/bushi/#{params[:bushi]}/bushido/remoteshell/#{params[:id]}/evt/write",
   :remoteshell_evt_error	=> "bu/bushi/#{params[:bushi]}/bushido/remoteshell/#{params[:id]}/evt/error"
@@ -58,19 +58,21 @@ re.add_topic_callback(:bushi) do |message|
 
     packet = re.packets(:open)
     packet = re.seen(packet)
-    re.publish(:remoteshell_cmd_open, packet, false, 2)
+    re.publish(:remoteshell, packet, false, 2)
   when 'offline'
     exit
   end
 end
 
-re.add_topic_callback(:remoteshell) do |message|
-  if message.payload != '' then
-    packet = re.decryse(message.payload)
-    puts packet.to_yaml
-  else
-    exit
-  end
+re.add_topic_callback(:remoteshell_evt_open) do |message|
+  packet = re.decryse(message.payload)
+  puts "remoteshell.opened #{packet.id}"
+end
+
+re.add_topic_callback(:remoteshell_evt_close) do |message|
+  packet = re.decryse(message.payload)
+  puts "remoteshell.closed #{packet.id}"
+  exit
 end
 
 re.add_topic_callback(:remoteshell_evt_read) do |message|
@@ -99,9 +101,7 @@ Thread.new {
 }
 
 END {
-  packet = re.packets(:close)
-  packet = re.seen(packet)
-  re.publish(:remoteshell_cmd_close, packet, false, 2)
+  re.publish(:remoteshell, nil, false, 2)
   loop do
     re.internals[:mqtt].mqtt_loop
   end
